365dc56274640e5896cff6865036e9692f6742e1
Tales Wallace Souza <tales.wallace@gmail.com>
2019-08-15 23:49:01 -0300

    Adicionado metodos put e delete no serviço rest spring

--- CRUD-spring/src/main/java/com/taleswsouza/produtos/CrudSpringApplication.java
+++ CRUD-spring/src/main/java/com/taleswsouza/produtos/CrudSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringApplication.class, args);
+    }
 
 }
--- CRUD-spring/src/main/java/com/taleswsouza/produtos/resource/ProdutoResource.java
+++ CRUD-spring/src/main/java/com/taleswsouza/produtos/resource/ProdutoResource.java
@@ -6,9 +6,11 @@ import com.taleswsouza.produtos.repository.ProdutoRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,17 +39,14 @@ public class ProdutoResource {
         produtoRepository.save(produto);
         return produto;
     }
-//
-//    @PUT
-//    @Path("{id}")
-//    public void atualizar(@PathParam("id") Integer id, Produto produto) {
-//        entityManager.merge(produto);
-//    }
-//
-//    @DELETE
-//    @Path("{id}")
-//    public void excluir(@PathParam("id") Integer id) {
-//        entityManager.remove(getProduto(id));
-//    }
-//
+
+    @PutMapping("/produtos/{id}")
+    public void atualizar(@PathVariable(value="id") Integer id, @RequestBody Produto produto) {
+        produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produtos/{id}")
+    public void excluir(@PathVariable(value="id") Integer id) {
+        produtoRepository.deleteById(id);
+    }
 }
